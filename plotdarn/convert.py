d010276d1e5003308aa43c172596e49cb8eb16f0
TeriForey <teri.forey@gmail.com>
2019-09-24 16:39:20 +0100

    Added more tests and docstring for convert_mag_single

--- plotdarn/convert.py
+++ plotdarn/convert.py
@@ -4,6 +4,15 @@ from spacepy.time import Ticktock
 
 
 def convert_mag_single(loc, dtime):
+    """
+    Convert a single location in geomagnetic coords into geodetic coords
+    :param loc:
+        location.Location object with longitude and latitude in geomagnetic coords
+    :param dtime:
+        either datetime object or parse-able string
+    :return:
+        location.Location object with longitude and latitude in geodetic coords
+    """
     coords = Coords([1, loc.lat, loc.lon], 'MAG', 'sph')
     coords.ticks = Ticktock(dtime, 'ISO')
     converted = coords.convert('GDZ', 'sph')
--- tests/test_convert.py
+++ tests/test_convert.py
@@ -15,3 +15,22 @@ def test_convert_single_not_a_loc():
     with pytest.raises(AttributeError):
         convert_mag_single([20, 30], "2012")
 
+
+def test_convert_single_not_a_datetime():
+    time = "2012-06-15 22:02"
+    res = convert_mag_single(north_pole, time)
+    assert res.lat == 80.2281362381121
+    assert res.lon == -72.41454428382853
+
+
+def test_convert_single_strdate_timezone():
+    time = "2012-06-15 22:02Z"
+    res = convert_mag_single(north_pole, time)
+    assert res.lat == 80.2281362381121
+    assert res.lon == -72.41454428382853
+
+
+def test_convert_single_unknown_date():
+    time = "This can't be a time"
+    with pytest.raises(ValueError):
+        convert_mag_single(north_pole, time)
