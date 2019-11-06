fd7638cdd24cbcd49029480eb027c97052dc56b7
TeriForey <teri.forey@gmail.com>
2019-11-06 10:50:23 +0000

    Changed conversions to use aacgmv2 package so they return the same results as the original IDL code

--- plotdarn/convert.py
+++ plotdarn/convert.py
@@ -1,10 +1,12 @@
 from .locations import Location
 from spacepy.coordinates import Coords
 from spacepy.time import Ticktock
+import aacgmv2
+import datetime as dt
 import numpy as np
 
 
-def convert_mag_single(loc, dtime):
+def loc_mag_to_geo(loc, dtime):
     """
     Convert a single location in geomagnetic coords into geodetic coords
     :param loc:
@@ -14,16 +16,23 @@ def convert_mag_single(loc, dtime):
     :return:
         location.Location object with longitude and latitude in geodetic coords
     """
-    coords = Coords([1, loc.lat, loc.lon], 'MAG', 'sph')
-    coords.ticks = Ticktock(dtime, 'ISO')
-    converted = coords.convert('GDZ', 'sph')
-    newloc = Location(converted.data[0, 1], converted.data[0, 2])
+    if isinstance(dtime, str):
+        try:
+            dtime = dt.datetime.strptime(dtime, "%Y-%m-%d %H:%M")
+        except ValueError:
+            try:
+                dtime = dt.datetime.strptime(dtime, "%Y-%m-%d %H:%M%z")
+            except ValueError:
+                raise ValueError("Unable to convert string to datetime")
+
+    converted = aacgmv2.convert_latlon(loc.lat, loc.lon, 100, dtime, code='A2G')
+    newloc = Location(converted[0], converted[1])
     return newloc
 
 
-def convert_mag_arr(latitudes, longitudes, dtime):
+def arr_mag_to_geo(latitudes, longitudes, dtime):
     """
-    Convert two arrays of latitudes and longitudes of geomagnetic coords in geodetic coords. Numpy array is returned
+    Convert two arrays of latitudes and longitudes of geomagnetic coords into geodetic coords. Numpy array is returned
     of lat, lon pairs, e.g. [[lat, lon], [lat, lon]]
     :param latitudes: ndarray
     :param longitudes: ndarray
@@ -32,12 +41,14 @@ def convert_mag_arr(latitudes, longitudes, dtime):
     """
     if len(latitudes) != len(longitudes):
         raise ValueError("Input latitude and longitude must be the same length!")
-    nvals = len(latitudes)
-    data = np.array([np.ones(nvals), latitudes, longitudes])
+    if isinstance(latitudes, np.ndarray):
+        if latitudes.shape != longitudes.shape:
+            raise ValueError('Arrays are not of equal length')
+        if len(latitudes.shape) > 1:
+            raise ValueError('Latitudes should be in a single dimension')
+    if isinstance(longitudes, np.ndarray):
+        if len(longitudes.shape) > 1:
+            raise ValueError('Longitudes should be in a single dimension')
 
-    times = [dtime for i in range(nvals)]
-
-    coords = Coords(data.T, 'MAG', 'sph')
-    coords.ticks = Ticktock(times, 'ISO')
-    converted = coords.convert('GDZ', 'sph')
-    return converted.data[:, [1, 2]]
+    converted = aacgmv2.convert_latlon_arr(latitudes, longitudes, 100, dtime, code='A2G')
+    return converted[0:2]
--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,5 +1,5 @@
 import pytest
-from plotdarn.convert import convert_mag_single, convert_mag_arr
+from plotdarn import convert
 from plotdarn.locations import north_pole
 from datetime import datetime
 import numpy as np
@@ -7,49 +7,49 @@ import numpy as np
 
 def test_convert_single():
     time = datetime(year=2012, month=6, day=15, hour=22, minute=2)
-    newloc = convert_mag_single(north_pole, time)
-    assert newloc.lat == 80.2281362381121
-    assert newloc.lon == -72.41454428382853
+    newloc = convert.loc_mag_to_geo(north_pole, time)
+    assert newloc.lat == 82.828317505718
+    assert newloc.lon == -84.22458117342626
 
 
 def test_convert_single_not_a_loc():
     with pytest.raises(AttributeError):
-        convert_mag_single([20, 30], "2012")
+        convert.loc_mag_to_geo([20, 30], "2012-06-15 22:02")
 
 
 def test_convert_single_not_a_datetime():
     time = "2012-06-15 22:02"
-    res = convert_mag_single(north_pole, time)
-    assert res.lat == 80.2281362381121
-    assert res.lon == -72.41454428382853
+    res = convert.loc_mag_to_geo(north_pole, time)
+    assert res.lat == 82.828317505718
+    assert res.lon == -84.22458117342626
 
 
 def test_convert_single_strdate_timezone():
     time = "2012-06-15 22:02Z"
-    res = convert_mag_single(north_pole, time)
-    assert res.lat == 80.2281362381121
-    assert res.lon == -72.41454428382853
+    res = convert.loc_mag_to_geo(north_pole, time)
+    assert res.lat == 82.828317505718
+    assert res.lon == -84.22458117342626
 
 
 def test_convert_single_unknown_date():
     time = "This can't be a time"
     with pytest.raises(ValueError):
-        convert_mag_single(north_pole, time)
+        convert.loc_mag_to_geo(north_pole, time)
 
 
 def test_convert_array():
     time = datetime(year=2012, month=6, day=15, hour=22, minute=2)
-    res = convert_mag_arr([north_pole.lat], [north_pole.lon], time)
-    np.testing.assert_array_equal(res, np.array([[80.2281362381121, -72.41454428382853]]))
+    res = convert.arr_mag_to_geo([north_pole.lat], [north_pole.lon], time)
+    np.testing.assert_array_equal(res, np.array([[82.828317505718], [-84.22458117342626]]))
 
 
 def test_convert_array_2vals():
     time = datetime(year=2012, month=6, day=15, hour=22, minute=2)
-    res = convert_mag_arr([north_pole.lat, north_pole.lat], [north_pole.lon, north_pole.lon], time)
+    res = convert.arr_mag_to_geo([north_pole.lat, north_pole.lat], [north_pole.lon, north_pole.lon], time)
     expected = np.array(
         [
-            [80.2281362381121, -72.41454428382853],
-            [80.2281362381121, -72.41454428382853]
+            [82.828317505718, 82.828317505718],
+            [-84.22458117342626, -84.22458117342626]
         ]
     )
     np.testing.assert_array_equal(res, expected)
@@ -59,11 +59,11 @@ def test_convert_array_np_input():
     time = datetime(year=2012, month=6, day=15, hour=22, minute=2)
     input_lat = np.array([north_pole.lat, north_pole.lat])
     input_lon = np.array([north_pole.lon, north_pole.lon])
-    res = convert_mag_arr(input_lat, input_lon, time)
+    res = convert.arr_mag_to_geo(input_lat, input_lon, time)
     expected = np.array(
         [
-            [80.2281362381121, -72.41454428382853],
-            [80.2281362381121, -72.41454428382853]
+            [82.828317505718, 82.828317505718],
+            [-84.22458117342626, -84.22458117342626]
         ]
     )
     np.testing.assert_array_equal(res, expected)
@@ -72,13 +72,13 @@ def test_convert_array_np_input():
 def test_convert_array_single_input():
     time = datetime(year=2012, month=6, day=15, hour=22, minute=2)
     with pytest.raises(TypeError):
-        convert_mag_arr(north_pole.lat, north_pole.lon, time)
+        convert.arr_mag_to_geo(north_pole.lat, north_pole.lon, time)
 
 
 def test_convert_array_uneven():
     time = datetime(year=2012, month=6, day=15, hour=22, minute=2)
     with pytest.raises(ValueError):
-        convert_mag_arr([north_pole.lat, north_pole.lat], [north_pole.lon], time)
+        convert.arr_mag_to_geo([north_pole.lat, north_pole.lat], [north_pole.lon], time)
 
 
 def test_convert_array_np_input_shape():
@@ -86,4 +86,4 @@ def test_convert_array_np_input_shape():
     input_lat = np.array([[north_pole.lat], [north_pole.lat]])
     input_lon = np.array([[north_pole.lon], [north_pole.lon]])
     with pytest.raises(ValueError):
-        convert_mag_arr(input_lat, input_lon, time)
+        convert.arr_mag_to_geo(input_lat, input_lon, time)
