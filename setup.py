130c6f50cb82de934c8e9c35c85408f8ce7ecd04
TeriForey <teri.forey@gmail.com>
2019-12-04 16:31:03 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('HISTORY.rst') as history_file:
 requirements = [
     'numpy>=1.16',
     'pvlib>=0.6',
-    'pydarn @ git+https://github.com/SuperDarn/pydarn@develop#egg=pydarn',
+    'pydarn',
     'matplotlib',
     'aacgmv2',
     'bokeh',
@@ -23,6 +23,10 @@ requirements = [
 setup_requirements = ['pytest-runner', ]
 
 test_requirements = ['pytest>=3', ]
+
+dependency_links = [
+    'git+https://github.com/SuperDarn/pydarn@develop#egg=pydarn'
+]
 
 setup(
     author="Teri Forey",
@@ -47,6 +51,7 @@ setup(
         ],
     },
     install_requires=requirements,
+    dependency_links=dependency_links,
     license="MIT license",
     long_description=readme + '\n\n' + history,
     include_package_data=True,
